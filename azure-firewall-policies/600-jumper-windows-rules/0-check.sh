c7cadf90c32beba7652f80d1f44f5b560e78abda
Phil Huang <phil.huang@microsoft.com>
2024-12-09 00:21:19 +0800

    Remove unused check script and add Azure Firewall Policy existence and tier checks

--- azure-firewall-policies/600-jumper-windows-rules/0-check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Checking Azure Firewall Policy is existing..."
-
-echo "Checking Azure Firewall Policies tier is Standard or Premium..."
-
--- azure-firewall-policies/65000-scratch/0-check.sh
+++ azure-firewall-policies/65000-scratch/0-check.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+source terraform.tfvars
+
 echo "Checking Azure Firewall Policy is existing..."
+if az network firewall policy show --name $FW_POLICY_NAME -g $RG_NAME; then
+    echo "Azure Firewall Policy exists."
+else
+    echo "Azure Firewall Policy does not exist."
+fi
 
 echo "Checking Azure Firewall Policies tier is Standard or Premium..."
+tier=$(az network firewall policy show --name $FW_POLICY_NAME -g $RG_NAME --query "sku.tier" -o tsv)
+if [ "$tier" == "Standard" ] || [ "$tier" == "Premium" ]; then
+    echo "Azure Firewall Policy tier is $tier."
+else
+    echo "Azure Firewall Policy tier is not Standard or Premium."
+fi
 
+echo "Initializing Terraform..."
+terraform init
