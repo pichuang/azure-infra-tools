1b0c64d26bf14463cedd4fb859267fbbbefdd5b6
Phil Huang <phil.huang@microsoft.com>
2025-11-23 21:36:58 +0800

    🆕 Add comments to clarify Certbot key type requirements and OpenSSL compatibility
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- letencryption-self-signed/1-certbot.sh
+++ letencryption-self-signed/1-certbot.sh
@@ -9,6 +9,10 @@ for dir in certbot-config certbot-logs certbot-work; do
     fi
 done
 
+# By default, Certbot issues ECDSA (EC) keys, but Azure Key Vault or the certificate store expects RSA keys by default, causing a mismatch during upload
+
+# If your OpenSSL version is 3.0 or above (like the one installed by Homebrew on macOS), you need to add the -legacy flag to enforce the use of the legacy encryption format, which Azure can understand.
+
 certbot certonly --manual \
     --preferred-challenges dns \
     --key-type rsa \
